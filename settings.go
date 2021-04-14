4a479e9c11536246963714f10b4fa7fb22163ce0
Flavio Castelli <fcastelli@suse.com>
2021-04-14 11:18:13 +0200

    Update policy settings
    Change the scaffolded code to reflect what the policy has to do

--- settings.go
+++ settings.go
@@ -6,10 +6,48 @@ import (
 	kubewarden "github.com/kubewarden/policy-sdk-go"
 
 	"fmt"
+	"regexp"
 )
 
+// A wrapper around the standard regexp.Regexp struct
+// that implements marshalling and unmarshalling
+type RegularExpression struct {
+	*regexp.Regexp
+}
+
+// Convenience method to build a regular expression
+func CompileRegularExpression(expr string) (*RegularExpression, error) {
+	nativeRegExp, err := regexp.Compile(expr)
+	if err != nil {
+		return nil, err
+	}
+	return &RegularExpression{nativeRegExp}, nil
+}
+
+// UnmarshalText satisfies the encoding.TextMarshaler interface,
+// also used by json.Unmarshal.
+func (r *RegularExpression) UnmarshalText(text []byte) error {
+	nativeRegExp, err := regexp.Compile(string(text))
+	if err != nil {
+		return err
+	}
+	r.Regexp = nativeRegExp
+	return nil
+}
+
+// MarshalText satisfies the encoding.TextMarshaler interface,
+// also used by json.Marshal.
+func (r *RegularExpression) MarshalText() ([]byte, error) {
+	if r.Regexp != nil {
+		return []byte(r.Regexp.String()), nil
+	}
+
+	return nil, nil
+}
+
 type Settings struct {
-	DeniedNames mapset.Set `json:"denied_names"`
+	DeniedLabels      mapset.Set                    `json:"denied_labels"`
+	ConstrainedLabels map[string]*RegularExpression `json:"constrained_labels"`
 }
 
 // Builds a new Settings instance starting from a validation
@@ -17,7 +55,8 @@ type Settings struct {
 // {
 //    "request": ...,
 //    "settings": {
-//       "denied_names": [...]
+//       "denied_labels": [...],
+//       "constrained_labels": { ... }
 //    }
 // }
 func NewSettingsFromValidationReq(payload []byte) (Settings, error) {
@@ -26,13 +65,15 @@ func NewSettingsFromValidationReq(payload []byte) (Settings, error) {
 
 	return newSettings(
 		payload,
-		"settings.denied_names")
+		"settings.denied_labels",
+		"settings.constrained_labels")
 }
 
 // Builds a new Settings instance starting from a Settings
 // payload:
 // {
-//    "denied_names": ...
+//    "denied_names": [ ... ],
+//    "constrained_labels": { ... }
 // }
 func NewSettingsFromValidateSettingsPayload(payload []byte) (Settings, error) {
 	if !gjson.ValidBytes(payload) {
@@ -41,40 +82,73 @@ func NewSettingsFromValidateSettingsPayload(payload []byte) (Settings, error) {
 
 	return newSettings(
 		payload,
-		"denied_names")
+		"denied_labels",
+		"constrained_labels")
 }
 
 func newSettings(payload []byte, paths ...string) (Settings, error) {
-	if len(paths) != 1 {
+	if len(paths) != 2 {
 		return Settings{}, fmt.Errorf("wrong number of json paths")
 	}
 
 	data := gjson.GetManyBytes(payload, paths...)
 
-	deniedNames := mapset.NewThreadUnsafeSet()
+	deniedLabels := mapset.NewThreadUnsafeSet()
 	data[0].ForEach(func(_, entry gjson.Result) bool {
-		deniedNames.Add(entry.String())
+		deniedLabels.Add(entry.String())
 		return true
 	})
 
+	constrainedLabels := make(map[string]*RegularExpression)
+	var err error
+	data[1].ForEach(func(key, value gjson.Result) bool {
+		var regExp *RegularExpression
+		regExp, err = CompileRegularExpression(value.String())
+		if err != nil {
+			return false
+		}
+
+		constrainedLabels[key.String()] = regExp
+		return true
+	})
+	if err != nil {
+		return Settings{}, err
+	}
+
 	return Settings{
-		DeniedNames: deniedNames,
+		DeniedLabels:      deniedLabels,
+		ConstrainedLabels: constrainedLabels,
 	}, nil
 }
 
-// No special check has to be done
-func (s *Settings) Valid() bool {
-	return true
+func (s *Settings) Valid() (bool, error) {
+	constrainedLabels := mapset.NewThreadUnsafeSet()
+
+	for label := range s.ConstrainedLabels {
+		constrainedLabels.Add(label)
+	}
+
+	constrainedAndDenied := constrainedLabels.Intersect(s.DeniedLabels)
+	if constrainedAndDenied.Cardinality() != 0 {
+		return false,
+			fmt.Errorf("These labels cannot be constrained and denied at the same time: %v", constrainedAndDenied)
+	}
+
+	return true, nil
 }
 
 func validateSettings(payload []byte) ([]byte, error) {
 	settings, err := NewSettingsFromValidateSettingsPayload(payload)
 	if err != nil {
-		return []byte{}, err
+		// this happens when one of the user-defined regular expressions are invalid
+		return kubewarden.RejectSettings(
+			kubewarden.Message(fmt.Sprintf("Provided settings are not valid: %v", err)))
 	}
 
-	if settings.Valid() {
+	valid, err := settings.Valid()
+	if valid {
 		return kubewarden.AcceptSettings()
 	}
-	return kubewarden.RejectSettings(kubewarden.Message("Provided settings are not valid"))
+	return kubewarden.RejectSettings(
+		kubewarden.Message(fmt.Sprintf("Provided settings are not valid: %v", err)))
 }
--- settings_test.go
+++ settings_test.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"testing"
+
+	kubewarden_testing "github.com/kubewarden/policy-sdk-go/testing"
 )
 
-func TestParsingSettingsWithAllValuesProvidedFromValidationReq(t *testing.T) {
+func TestParseValidSettings(t *testing.T) {
 	request := `
 	{
 		"request": "doesn't matter here",
 		"settings": {
-			"denied_names": [ "foo", "bar" ]
+			"denied_labels": [ "foo", "bar" ],
+			"constrained_labels": {
+				"cost-center": "cc-\\d+"
+			}
 		}
 	}
 	`
@@ -20,47 +26,124 @@ func TestParsingSettingsWithAllValuesProvidedFromValidationReq(t *testing.T) {
 		t.Errorf("Unexpected error %+v", err)
 	}
 
-	expected := []string{"foo", "bar"}
-	for _, exp := range expected {
-		if !settings.DeniedNames.Contains(exp) {
+	expected_denied_labels := []string{"foo", "bar"}
+	for _, exp := range expected_denied_labels {
+		if !settings.DeniedLabels.Contains(exp) {
 			t.Errorf("Missing value %s", exp)
 		}
 	}
+
+	re, found := settings.ConstrainedLabels["cost-center"]
+	if !found {
+		t.Error("Didn't find the expected constrained label")
+	}
+
+	expected_regexp := `cc-\d+`
+	if re.String() != expected_regexp {
+		t.Errorf("Execpted regexp to be %v - got %v instead",
+			expected_regexp, re.String())
+	}
 }
 
-func TestParsingSettingsWithNoValueProvided(t *testing.T) {
+func TestParseSettingsWithInvalidRegexp(t *testing.T) {
 	request := `
 	{
 		"request": "doesn't matter here",
 		"settings": {
+			"denied_labels": [ "foo", "bar" ],
+			"constrained_labels": {
+				"cost-center": "cc-[a+"
+			}
 		}
 	}
 	`
 	rawRequest := []byte(request)
 
-	settings, err := NewSettingsFromValidationReq(rawRequest)
-	if err != nil {
-		t.Errorf("Unexpected error %+v", err)
-	}
-
-	if settings.DeniedNames.Cardinality() != 0 {
-		t.Errorf("Expecpted DeniedNames to be empty")
+	_, err := NewSettingsFromValidationReq(rawRequest)
+	if err == nil {
+		t.Errorf("Didn'g get expected error")
 	}
 }
 
-func TestSettingsAreValid(t *testing.T) {
+func TestDetectValidSettings(t *testing.T) {
 	request := `
 	{
+		"denied_labels": [ "foo", "bar" ],
+		"constrained_labels": {
+			"cost-center": "cc-\\d+"
+		}
 	}
 	`
 	rawRequest := []byte(request)
-
-	settings, err := NewSettingsFromValidateSettingsPayload(rawRequest)
+	responsePayload, err := validateSettings(rawRequest)
 	if err != nil {
 		t.Errorf("Unexpected error %+v", err)
 	}
 
-	if !settings.Valid() {
-		t.Errorf("Settings are reported as not valid")
+	var response kubewarden_testing.SettingsValidationResponse
+	if err := json.Unmarshal(responsePayload, &response); err != nil {
+		t.Errorf("Unexpected error: %+v", err)
+	}
+
+	if !response.Valid {
+		t.Errorf("Expected settings to be valid: %s", response.Message)
+	}
+}
+
+func TestDetectNotValidSettingsDueToBrokenRegexp(t *testing.T) {
+	request := `
+	{
+		"denied_labels": [ "foo", "bar" ],
+		"constrained_labels": {
+			"cost-center": "cc-[a+"
+		}
+	}
+	`
+	rawRequest := []byte(request)
+	responsePayload, err := validateSettings(rawRequest)
+	if err != nil {
+		t.Errorf("Unexpected error %+v", err)
+	}
+
+	var response kubewarden_testing.SettingsValidationResponse
+	if err := json.Unmarshal(responsePayload, &response); err != nil {
+		t.Errorf("Unexpected error: %+v", err)
+	}
+
+	if response.Valid {
+		t.Error("Expected settings to not be valid")
+	}
+
+	if response.Message != "Provided settings are not valid: error parsing regexp: missing closing ]: `[a+`" {
+		t.Errorf("Unexpected validation error message: %s", response.Message)
+	}
+}
+
+func TestDetectNotValidSettingsDueToConflictingLabels(t *testing.T) {
+	request := `
+	{
+		"denied_labels": [ "foo", "bar", "cost-center" ],
+		"constrained_labels": {
+			"cost-center": ".*"
+		}
+	}
+	`
+	rawRequest := []byte(request)
+	responsePayload, err := validateSettings(rawRequest)
+	if err != nil {
+		t.Errorf("Unexpected error %+v", err)
+	}
+
+	var response kubewarden_testing.SettingsValidationResponse
+	if err := json.Unmarshal(responsePayload, &response); err != nil {
+		t.Errorf("Unexpected error: %+v", err)
+	}
+
+	if response.Valid {
+		t.Error("Expected settings to not be valid")
+	}
+
+	if response.Message != "Provided settings are not valid: These labels cannot be constrained and denied at the same time: Set{cost-center}" {
+		t.Errorf("Unexpected validation error message: %s", response.Message)
 	}
 }
