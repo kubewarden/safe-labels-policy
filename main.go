05aaf3419a9b49e1e44da388adc8ed7264cf1072
Flavio Castelli <fcastelli@suse.com>
2021-06-03 18:28:42 +0200

    Update to latest version of the Kubewarden SDK
    This simplifies the process of registering waPC functions

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	kubewarden "github.com/kubewarden/policy-sdk-go"
 	wapc "github.com/wapc/wapc-guest-tinygo"
 )
 
@@ -9,6 +8,5 @@ func main() {
 	wapc.RegisterFunctions(wapc.Functions{
 		"validate":          validate,
 		"validate_settings": validateSettings,
-		"protocol_version":  kubewarden.ProtocolVersionCallback,
 	})
 }
