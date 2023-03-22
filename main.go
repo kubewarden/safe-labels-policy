86b55a7ef172c843e1e7ebc9cbcf1f260dc22071
José Guilherme Vanz <jguilhermevanz@suse.com>
2023-03-22 15:38:35 -0300

    Remove unused variables.
    Removes the unused logger variables. This is a finding from the golang
    linter.
    
    Signed-off-by: José Guilherme Vanz <jguilhermevanz@suse.com>

--- main.go
+++ main.go
@@ -1,17 +1,7 @@
 package main
 
 import (
-	onelog "github.com/francoispqt/onelog"
-	kubewarden "github.com/kubewarden/policy-sdk-go"
 	wapc "github.com/wapc/wapc-guest-tinygo"
-)
-
-var (
-	logWriter = kubewarden.KubewardenLogWriter{}
-	logger    = onelog.New(
-		&logWriter,
-		onelog.ALL, // shortcut for onelog.DEBUG|onelog.INFO|onelog.WARN|onelog.ERROR|onelog.FATAL
-	)
 )
 
 func main() {
